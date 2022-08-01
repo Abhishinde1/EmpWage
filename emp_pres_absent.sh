db2687101f9a2f540a81e8869840d2adf5717128
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-01 15:50:34 +0530

    employeedailywage

--- emp_pres_absent.sh
+++ emp_pres_absent.sh
@@ -1,20 +1,19 @@
-#!/bin/bash
+#!/bin/bash -x
 
 perhours=20
-
-
-empcheck=$(($RANDOM%2))
-if (($empcheck==1))
+empcheck=$(($RANDOM%3))
+if (( $empcheck==1 ))
 then
 echo "Emolyee is fulltime"
        emphrs=8
 elif(($empcheck==0))
 then
-       emphrs=4
 echo "Employee is parttime"
+      emphrs=4
 else
 echo "employee is absent"
       emphrs=0
+fi
 salary=$(($emphrs * $perhours))
 echo "the salary is $salary"
-fi
+
